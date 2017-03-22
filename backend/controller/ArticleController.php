80ac54cfc297b0fa52ea30f64eb29ec41d25f740
BR-0309 <benjaminraison@rocketmail.com>
2017-03-22 11:30:11 +0100

    Initial commit. Contains boilerplate and sort-of frameworks

---
+++ backend/controller/ArticleController.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once '../repository/UserRepository.php';
+
+class ArticleController
+{   
+
+}
---
+++ backend/lib/Dispatcher.php
@@ -0,0 +1,26 @@
+<?php
+class Dispatcher
+{
+    public static function dispatch()
+    {
+        $uri = $_SERVER['REQUEST_URI'];
+        $uri = strtok($uri, '?');
+        $uri = trim($uri, '/');
+        $uriFragments = explode('/', $uri);
+
+        $controllerName = 'DefaultController';
+        if (!empty($uriFragments[1])) {
+            $controllerName = $uriFragments[1];
+            $controllerName = ucfirst($controllerName);
+            $controllerName .= 'Controller';
+        }
+        $method = 'index';
+        if (!empty($uriFragments[2])) {
+            $method = $uriFragments[2];
+        }
+        $args = array_slice($uriFragments, 3);
+        require_once "../Server/controller/$controllerName.php";
+        $controller = new $controllerName();
+        $controller->$method();
+    }
+}
