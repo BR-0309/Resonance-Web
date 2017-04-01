7ead7c00fda6c1ac2fcfac054bd1728ae9520497
BR-0309 <benjaminraison@rocketmail.com>
2017-04-02 00:00:49 +0200

    Contact form now contacts me. Renamed view to index to make for nicer URLs

--- backend/controller/ContactController.php
+++ backend/controller/ContactController.php
@@ -1,4 +1,39 @@
 <?php
+require_once 'repository/ContactRepository.php';
 class ContactController {
+
+    public function index() {
+        if(isset($_POST['name'], $_POST['email'], $_POST['text'])) {
+            if(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+                $this->error('Invalid e-mail address');
+            }
+            if(strlen($_POST['name']) < 4) {
+                $this->error('Name too short');
+            }
+            if(strlen($_POST['text']) < 20) {
+                $this->error('Message too short');
+            }
+            $repo = new ContactRepository();
+            $repo->create($_POST['name'], $_POST['email'], $_POST['text']);
+            echo '{
+                "sucess": {
+                    "code": 200,
+                    "message": "Message sent!"
+                 }
+              }';
+        } else {
+            $this->error('Missing parameters');
+        }
+    }
+
+    private function error($message) {
+        echo '{
+                "error": {
+                    "code": 400,
+                    "message": "' . $message . '"
+                 }
+              }';
+        exit();
+    }
 
 }
---
+++ backend/repository/ContactRepository.php
@@ -0,0 +1,16 @@
+<?php
+require_once 'lib/Repository.php';
+class ContactRepository extends Repository
+{
+    protected $tableName = 'contact';
+
+    public function create($name, $email, $text)
+    {
+        $query = "INSERT INTO contact (name, email, message) VALUES (?, ?, ?)";
+        $statement = ConnectionHandler::getConnection()->prepare($query);
+        $statement->bind_param('sss', $name, $email, $text);
+        if (!$statement->execute()) {
+            throw new Exception($statement->error);
+        }
+    }
+}
