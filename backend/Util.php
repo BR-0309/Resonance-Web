c69fae0c523873dd896d3d6712ef33c694ef0798
BR-0309 <benjaminraison@rocketmail.com>
2017-04-06 21:31:08 +0200

    Stopped http resources trying to render

--- backend/Util.php
+++ backend/Util.php
@@ -29,7 +29,9 @@ class Util
 
     public static function allowsXFrame($url)
     {
-
+        if(substr( $url, 0, 7 ) === "http://") {
+            return false;
+        }
         file_get_contents($url);
         foreach ($http_response_header as $value) {
             if (strcmp($value, "X-Frame-Options: SAMEORIGIN") == 0 || strcmp($value, "X-Frame-Options: DENY") == 0) {
