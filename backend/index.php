567ca7b4ad50f42566377375d2b9ec5c20cf0a3e
BR-0309 <benjaminraison@rocketmail.com>
2017-04-07 11:50:44 +0200

    Commented out error reporting

--- backend/index.php
+++ backend/index.php
@@ -6,8 +6,8 @@ function handleError($exception)
     Util::returnMessage(500, "Unknown error occurred");
 }
 set_error_handler('handleError');
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+/*error_reporting(E_ALL);
+ini_set('display_errors', 1);*/
 require_once 'lib/Router.php';
 $router = new Router();
 $router->route();
